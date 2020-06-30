0fe7b0853c1212e31190b0bcd199c1c2f7720d6f
Jacob Thompson <thampsen@gmail.com>
2020-06-30 23:09:43 +0100

    init commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const shell = require('shelljs')
+var myArgs = process.argv.slice(2);
+
+if (process.argv.length <= 2) {
+  console.log("Please give this folder a name");
+  process.exit(-1);
+}
+
+shell.exec(`mkdir ${myArgs[0]} && cd ${myArgs[0]}`)
+shell.exec(`git clone git@github.com:thompson-mcdonald/verlay001.git ${myArgs[0]} && cd ${myArgs[0]} && rm -rf .git && git init`)
