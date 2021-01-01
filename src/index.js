0440756eead6ff492741d7e8357b56c3c75e317a
James <jim.me.ryan@gmail.com>
2021-01-01 17:21:23 +0000

    could be wrong idk shell or how to test

--- src/index.js
+++ src/index.js
@@ -8,4 +8,10 @@ if (process.argv.length <= 2) {
 }
 
 shell.exec(`mkdir ${myArgs[0]} && cd ${myArgs[0]}`)
-shell.exec(`git clone git@github.com:thompson-mcdonald/verlay001.git ${myArgs[0]} && cd ${myArgs[0]} && rm -rf .git && git init`)
+
+if ($n < 2) {
+  shell.exec(`git clone git@github.com:thompson-mcdonald/verlay001.git ${myArgs[0]} && cd ${myArgs[0]} && rm -rf .git && git init`)
+} else {
+  shell.exec(`git clone --single-branch --branch redux git@github.com:thompson-mcdonald/verlay001.git ${myArgs[0]} && cd ${myArgs[0]} && rm -rf .git && git init`)
+}
+
